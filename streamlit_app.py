1fc7ae814be54f65afb2ea61f3f33a2b2a4ac83f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-04-25 09:43:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
+st.title('🐼 Pandas - A minimum working example')
 
-st.write('Hello world!')
+if st.button('Load data'):
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv')
+  st.write(df)
+else:
+  st.info('Click on the button ')
