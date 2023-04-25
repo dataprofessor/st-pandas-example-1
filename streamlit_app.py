2769ac68f5fff0dbb7c47ae3dc1632adb4d6dd72
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-04-25 09:49:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,11 @@ import pandas as pd
 
 st.title('🐼 Pandas - A minimum working example')
 
+placeholder = st.empty()
+
 if st.button('Load data'):
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv')
-  st.write(df)
+  placeholder.write(df)
 else:
   st.info('Click on the button ')
+  placeholder.write()
